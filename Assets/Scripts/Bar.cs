bdc8525de0624b3ca69b534b3476951efc1802a9
nano Lopez <nano.lopez.84@gmail.com>
2022-01-29 16:36:12 -0300

    +Bar sprite

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -8,20 +8,32 @@ public class Bar : MonoBehaviour
     // -100 to +100
     public int CurrentCharge;
 
+    public GameObject Needle;
+
     // Start is called before the first frame update
     void Start()
     {
-        CurrentCharge = 20;
+        //Needle.transform.position = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        updateNeedlePosition();
     }
 
     public int Charge(int charge)
     {
         CurrentCharge = Mathf.Clamp(CurrentCharge + charge, CHARGE_MIN, CHARGE_MAX);
         return CurrentCharge;
+    }
+
+    void updateNeedlePosition()
+    {
+        Vector3 pos = Needle.transform.position;
+
+        // 0.85 porque coso
+        pos.x = gameObject.transform.position.x + (CurrentCharge / 100.0f) * 0.85f;
+        Needle.transform.position = pos;
     }
 }
--- Assets/Scripts/LeoTest.cs
+++ Assets/Scripts/LeoTest.cs
@@ -6,7 +6,6 @@ public class LeoTest : MonoBehaviour
 {
     public Bar Bar;
     public Button Button;
-    public GameObject Blood;
 
     // Start is called before the first frame update
     void Start()
