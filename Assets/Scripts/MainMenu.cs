7ff312ff1f00f24a40eac11b2db1a0a5a893298a
nano Lopez <nano.lopez.84@gmail.com>
2022-01-31 18:07:49 -0300

    +Credits Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,4 +7,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("FinalLevel");
     }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    }
 }
