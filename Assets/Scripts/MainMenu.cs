62c46fc10fa4587b947fe0ae6c522e4cf6900807
nano Lopez <nano.lopez.84@gmail.com>
2022-01-31 13:36:57 -0300

    Play button fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +5,6 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("FinalLevel");
     }
 }
